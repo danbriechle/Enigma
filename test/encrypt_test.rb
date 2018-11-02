d5948f1848015f2a548723ca656b9916a6bec761
Daniel Briechle <danbriechle@hotmail.com>
2018-11-02 17:29:00 -0600

    Created beginning framework for encrypt and decrypt classes, Simple cov, testhelper and gemfile inplace

---
+++ test/encrypt_test.rb
@@ -0,0 +1,10 @@
+require_relative '../test/test_helper'
+
+
+class EncryptTest < Minitest::Test
+  def test_it_exists
+
+  end
+end
+# initialized with date object, key object, maybe message object
+# decrypt and encrypt may become a shift class not sure yet
